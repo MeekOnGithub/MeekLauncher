29189d8f6831e7ea90dda8912de421a6a7f6f099
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-10-22 15:36:28 +0800

    Update launch.py

--- core/launch.py
+++ core/launch.py
@@ -16,7 +16,7 @@ def gethighoptions(js: list):
     temp = ""
     for i in js:
         if type(i) == str:
-            temp += getoptions(i)+" "
+            temp += getoptions(i) + " "
         else:
             break
     return temp
@@ -24,6 +24,7 @@ def gethighoptions(js: list):
 
 def launch(game_directory: str = ".minecraft", version_name: str = None, java: str = "java",
            auth_player_name: str = "player"):
+    cps = []
     if version_name is None:
         return 0
     if not (os.path.exists(game_directory) and os.path.exists(
@@ -40,18 +41,24 @@ def launch(game_directory: str = ".minecraft", version_name: str = None, java: s
            f"-Dlog4j2.formatMsgNoLookups=true -cp "
     for i in ver_json['libraries']:
         try:
-            temp += (os.path.realpath(
-                os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) + ";")
-            print((os.path.realpath(
-                os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) + ";"))
+            if not os.path.realpath(
+                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) in cps:
+                cps.append(os.path.realpath(
+                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']))
+                print((os.path.realpath(
+                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) + ";"))
         except KeyError:
             try:
                 print("Special: ", i['downloads']['classifiers']['natives-windows']['path'])
-                jar = os.path.realpath(os.path.realpath(game_directory) + "/libraries/" + i['downloads']['classifiers']['natives-windows']['path'])
-                core.mkdir.make_long_dir(os.path.realpath(f'{os.path.realpath(game_directory)}/versions/{version_name}/natives-windows-x86_64'))
+                jar = os.path.realpath(
+                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['classifiers']['natives-windows'][
+                        'path'])
+                core.mkdir.make_long_dir(os.path.realpath(
+                    f'{os.path.realpath(game_directory)}/versions/{version_name}/natives-windows-x86_64'))
                 zip_file = zipfile.ZipFile(jar)
                 for names in zip_file.namelist():
-                    zip_file.extract(names, os.path.realpath(f'{os.path.realpath(game_directory)}/versions/{version_name}/natives-windows-x86_64'))
+                    zip_file.extract(names, os.path.realpath(
+                        f'{os.path.realpath(game_directory)}/versions/{version_name}/natives-windows-x86_64'))
 
             except KeyError:
                 name = i["name"].split(":")  # <package>:<name>:<version>
@@ -62,12 +69,12 @@ def launch(game_directory: str = ".minecraft", version_name: str = None, java: s
                     else:
                         tmp += j
                 name[0] = tmp
-                temp += os.path.realpath(
-                    f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;")  # <package>/<name>/<version>/<name>-<version>.jar
-                print(os.path.realpath(
-                    f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;")
-                    # <package>/<name>/<version>/<name>-<version>.jar
-                )
+                if not os.path.realpath(
+                        os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) in cps:
+                    cps.append(os.path.realpath(
+                        f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;"))  # <package>/<name>/<version>/<name>-<version>.jar
+                    print(os.path.realpath(
+                        f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;"))
 
     assets_root = os.path.abspath(game_directory + "\\assets")
     assets_index_name = ver_json["assetIndex"]["id"]
@@ -78,10 +85,16 @@ def launch(game_directory: str = ".minecraft", version_name: str = None, java: s
     user_properties = "{}"
     auth_session = "{}"
 
+    cp = ""
+    for i in cps:
+        cp += i + ';'
+
     try:
-        return f'{temp}{os.path.realpath(f"{game_directory}/versions/{version_name}/{version_name}.jar")} {ver_json["mainClass"]} ' + eval(
+        return f'{temp} {cp}{os.path.realpath(f"{game_directory}/versions/{version_name}/{version_name}.jar")} {ver_json["mainClass"]} ' + eval(
             "f'" + getoptions(ver_json["minecraftArguments"] + "'"))
     except KeyError:
 
-        return f'{temp}{os.path.realpath(f"{game_directory}/versions/{version_name}/{version_name}.jar")} {ver_json["mainClass"]} ' + eval('f"'+gethighoptions(ver_json["arguments"]["game"])+'"')
+        return f'{temp} {cp}{os.path.realpath(f"{game_directory}/versions/{version_name}/{version_name}.jar")} {ver_json["mainClass"]} ' + eval(
+            'f"' + gethighoptions(ver_json["arguments"]["game"]) + '"')
+
 
