ee293b9a051ce2d33b40d4d83cbc46c06c0a56cd
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-10-22 19:24:20 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,16 +1,21 @@
 @echo off
 color 8e
 echo Welcome to FMCL build batchfile by Sharll. Pack by pyinstaller.
-set /p continue=Pack must install pyinstaller and requests. Main.spec and all the things in ~/dist and ~/build will be deleted! Would you like to install?(y/n)
+echo Pack must install pyinstaller and requests. Main.spec and all the things in ~/dist and ~/build will be deleted! 
+set /p continue=Would you like to install?(y/n):
 if not %continue%==y (
 echo Because you decide not to install, the program must be quit.
 pause
 exit
 )
-rd /s /q build
+if EXIST build rd /s /q build
+if EXIST main.exe del main.exe
+if EXIST main.spec del main.spec
 set pip="C:/Users/%Username%/Appdata/local/Programs/Python/Python310/Scripts/pip" 
 %pip% install pyinstaller
 %pip% install requests
 pyinstaller -F -w -i icon.ico --clean --add-data="icon.ico;." --distpath . main.py
+if EXIST build rd /s /q build
+if EXIST main.spec del main.spec
 echo done.
 pause
