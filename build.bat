2c9aa91fe7ec34a4858c83ec1f3d5f570c8323c1
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-10-22 16:11:37 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -10,6 +10,6 @@ exit
 set pip="C:/Users/%Username%/Appdata/local/Programs/Python/Python310/Scripts/pip" 
 %pip% install pyinstaller
 %pip% install requests
-pyinstaller -F -w basic.py
+pyinstaller -F -w main.py
 echo done.
 pause
