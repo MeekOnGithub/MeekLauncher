7404a52765da45cbf1c7b892b22f4593fa649465
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-10-22 14:52:43 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,15 @@
 @echo off
-echo First Minecraft Launcher by sharll. Packing on Pyinstaller.
-pip install requests
-pip install Pyinstaller
-pyinstaller -F basic.py
+color 8e
+echo Welcome to FMCL build batchfile by Sharll. Pack by pyinstaller.
+set /p continue=Build must install pyinstaller and requests. Would you like to install?(y/n)
+if not %continue%==y (
+echo Because you decide not to install, the program must be quit.
+pause
+exit
+)
+set pip="C:/Users/%Username%/Appdata/local/Programs/Python/Python310/Scripts/pip" 
+%pip% install pyinstaller
+%pip% install requests
+pyinstaller -F -w basic.py
 echo done.
 pause
