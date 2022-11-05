ac1d4cc91d1698de22d83500522cc3defd974301
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-11-05 09:54:14 +0800

    Add files via upload

--- core/download.py
+++ core/download.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import threading
 import zipfile
+import tkinter.messagebox
 
 import requests
 
@@ -10,7 +11,7 @@ import core.mkdir
 
 paths = []
 urls = []
-nativesjar=[]
+nativesjar = []
 
 
 def get_ver_jar_url(version: str):
@@ -19,68 +20,57 @@ def get_ver_jar_url(version: str):
             return i["url"]
 
 
-def get_task(version_name: str, version: str, mcpath: str, useBMCLAPI: bool):
-    global paths, urls
+def get_task(version_name: str, version: str, mcpath: str):
+    assets = "https://resources.download.minecraft.net"
+
+    global paths, urls, nativesjar
     paths = []
     urls = []
-    if useBMCLAPI:
-        assets = "https://bmclapi2.bangbang93.com/assets"
-        libraries = "https://bmclapi2.bangbang93.com/maven"
-        forge = "https://bmclapi2.bangbang93.com/maven"
-        fabricmeta = "https://bmclapi2.bangbang93.com/fabric-meta"
-        fabricmaven = "https://bmclapi2.bangbang93.com/maven"
-    else:
-        assets = "https://resources.download.minecraft.net"
-        libraries = "https://libraries.minecraft.net/"
-        forge = "https://files.minecraftforge.net/maven"
-        fabricmeta = "https://meta.fabricmc.net"
-        fabricmaven = "https://maven.fabricmc.net"
+    nativesjar = []
 
     if os.path.exists(f"{mcpath}/versions/{version_name}"):
-        print("Game is already installed. Please change a name.")
-        return 0
+        tkinter.messagebox.showinfo("Info", "The folder is exist. Launch fixing mode.")
 
-    print("0 Create Game Folder")
-    os.mkdir(f"{mcpath}/versions/{version_name}")
+    # create game folder
+    core.mkdir.make_long_dir(f"{mcpath}/versions/{version_name}")
 
-    print("1 Download version json")
-    print(get_ver_jar_url(version))
+    # download version json
     with open(f"{mcpath}/versions/{version_name}/{version_name}.json", "wb") as file:
         file.write(requests.get(get_ver_jar_url(version)).content)
     ver_json = json.load(open(f"{mcpath}/versions/{version_name}/{version_name}.json"))
 
-    print("2 Download version jar")
-    with open(f"{mcpath}/versions/{version_name}/{version_name}.jar", "wb") as file:
-        file.write(requests.get(f"{ver_json['downloads']['client']['url']}").content)
+    # add version.jar to task
+    urls.append(f"{ver_json['downloads']['client']['url']}")
+    paths.append(f"{mcpath}/versions/{version_name}/{version_name}.jar")
 
-    print("3 Download natives")
+    # natives
     for i in ver_json["libraries"]:
-        try:
+
+
+        if "artifact" in i["downloads"]:
             urls.append(i["downloads"]["artifact"]["url"])
-            paths.append(f"{mcpath}/libraries/{i['downloads']['artifact']['path']}")
-        except KeyError:
-            try:
-                print("Special native!")
-                urls.append(i["downloads"]["classifiers"]["natives-windows"]["url"])
-                paths.append(f'{mcpath}/libraries/{i["downloads"]["classifiers"]["natives-windows"]["path"]}')
-                nativesjar.append(f'{mcpath}/libraries/{i["downloads"]["classifiers"]["natives-windows"]["path"]}')
+            paths.append(f'{mcpath}/libraries/{i["downloads"]["artifact"]["path"]}')
 
-            except KeyError:
-                print("Sprcial:", i)
+        if ("classifiers" in i["downloads"]) and "natives-windows" in i["downloads"]["classifiers"]:
+            print(i)
+            urls.append(i["downloads"]["classifiers"]["natives-windows"]["url"])
+            paths.append(f'{mcpath}/libraries/{i["downloads"]["classifiers"]["natives-windows"]["path"]}')
+            nativesjar.append(f'{mcpath}/libraries/{i["downloads"]["classifiers"]["natives-windows"]["path"]}')
+        # if "extract" in i:
+        #     print(i)
 
-    print("4 Download assets index")
+    # assets
     core.mkdir.make_dir(f"{mcpath}/assets/indexes")
     core.mkdir.make_dir(f"{mcpath}/assets/objects")
     with open(f"{mcpath}/assets/indexes/{ver_json['assetIndex']['id']}.json", "wb") as file:
         file.write(requests.get(ver_json['assetIndex']['url']).content)
     index_json = json.load(open(f"{mcpath}/assets/indexes/{ver_json['assetIndex']['id']}.json"))
-
     for i in index_json["objects"]:
         now_hash = index_json["objects"][i]["hash"]
         urls.append(f"{assets}/{now_hash[0:2]}/{now_hash}")
         paths.append(f"{mcpath}/assets/objects/{now_hash[0:2]}/{now_hash}")
 
-    print("5 Download log4j2.xml file")
+    # log4j config
     core.mkdir.make_long_dir(f"{mcpath}\\versions\\{version_name}")
     urls.append(ver_json["logging"]["client"]["file"]["url"])
     paths.append(f"{mcpath}\\versions\\{version_name}\\log4j2.xml")
@@ -110,20 +100,19 @@ def multprocessing_task(tasks: list, function, cores: int, join: bool = True):
 def download_one(i):
     url = i[0]
     path = i[1]
-    print(f"Downloading {url} to {path}")
     core.mkdir.make_long_dir(os.path.dirname(path))
     open(os.path.realpath(path), "wb").write(requests.get(url).content)
-    print(f"Download {url} to {path} done.")
 
 
 def download(ver_name: str, version: str, mcpath: str, threads: int):
-    get_task(ver_name, version, mcpath, False)
+    get_task(ver_name, version, mcpath)
     tasks = []
     for i in range(len(urls)):
         tasks.append((urls[i], paths[i]))
     multprocessing_task(tasks, download_one, threads, True)
-    print("6 Unzip natives(dll)")
+
     for i in nativesjar:
+        print(i)
         jar = os.path.realpath(i)
         core.mkdir.make_long_dir(os.path.realpath(
             f'{os.path.realpath(mcpath)}/versions/{ver_name}/natives-windows-x86_64'))
--- core/launch.py
+++ core/launch.py
@@ -1,15 +1,9 @@
 import json
 import os
-import zipfile
-
-import core.mkdir
 
 
 def getoptions(js: str):
-    temp = ''
-    for i in js.split("$"):
-        temp += i
-    return temp
+    return js.replace("$", "")
 
 
 def gethighoptions(js: list):
@@ -22,8 +16,8 @@ def gethighoptions(js: list):
     return temp
 
 
-def launch(game_directory: str = ".minecraft", version_name: str = None, java: str = "java",
-           auth_player_name: str = "player"):
+def launch(game_directory: str = ".minecraft", version_name: str = None, java: str = "java", auth_player_name: str = "player"):
+    game_directory = os.path.realpath(game_directory)
     cps = []
     if version_name is None:
         return 0
@@ -31,55 +25,34 @@ def launch(game_directory: str = ".minecraft", version_name: str = None, java: s
             f"{game_directory}/versions/{version_name}/{version_name}.json")):
         return 1
     ver_json = json.load(open(f"{game_directory}\\versions\\{version_name}\\{version_name}.json"))
-    temp = f"{java} -Dfile.encoding=utf-8 -Dminecraft.client.jar={game_directory}\\versions\\{version_name}\\{version_name}.jar " \
-           f"-Djava.library.path={game_directory}\\versions\\{version_name}\\natives-windows-x86_64 " \
-           f"-XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:-DontCompileHugeMethods " \
+    temp = f"{java} -Dfile.encoding=GB18030 -Dminecraft.client.jar={game_directory}\\versions\\{version_name}\\{version_name}.jar " \
+           f"-XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -Djava.library.path={game_directory}\\versions\\{version_name}\\natives-windows-x86_64 " \
+           f"-XX:G1NewSizePercent=20 -XX:-DontCompileHugeMethods " \
            f"-XX:G1ReservePercent=20 -XX:G1HeapRegionSize=16m -XX:-UseAdaptiveSizePolicy -XX:-OmitStackTraceInFastThrow " \
            f"-Dfml.ignoreInvalidMinecraftCertificates=true -Dfml.ignorePatchDiscrepancies=true -Djava.rmi.server.useCodebaseOnly=true " \
            f"-Dcom.sun.jndi.rmi.object.trustURLCodebase=false -Dcom.sun.jndi.cosnaming.object.trustURLCodebase=false " \
            f"-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump " \
-           f"-Dlog4j2.formatMsgNoLookups=true -cp "
+           f"-Dlog4j2.formatMsgNoLookups=true -Dlog4j.configurationFile={game_directory}\\versions\\{version_name}\\log4j2.xml -cp"
     for i in ver_json['libraries']:
-        try:
-            if not os.path.realpath(
-                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) in cps:
+        if "name" in i:
+            print(i)
+            name = i["name"].split(":")  # <package>:<name>:<version>
+            name[0] = name[0].replace(".", "/")
+            if not os.path.realpath(game_directory + "/libraries/" + i["name"]) in cps:
                 cps.append(os.path.realpath(
-                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']))
-                print((os.path.realpath(
-                    os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) + ";"))
-        except KeyError:
-            try:
-                print("Special: ", i['downloads']['classifiers']['natives-windows']['path'])
-            except KeyError:
-                try:
-                    print(i)
-                    name = i["name"].split(":")  # <package>:<name>:<version>
-                    tmp = ""
-                    for j in name[0]:
-                        if j == ".":
-                            tmp += "/"
-                        else:
-                            tmp += j
-                    name[0] = tmp
-                    if not os.path.realpath(os.path.realpath(game_directory) + "/libraries/" + i['downloads']['artifact']['path']) in cps:
-                        cps.append(os.path.realpath(
-                            f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;"))  # <package>/<name>/<version>/<name>-<version>.jar
-                        print(os.path.realpath(
-                            f"{os.path.realpath(game_directory)}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar;"))
-                except KeyError:
-                    print("pass!")
-                    pass
+                    f"{game_directory}\\libraries\\{name[0]}\\{name[1]}\\{name[2]}\\{name[1]}-{name[2]}.jar"))  # <package>/<name>/<version>/<name>-<version>.jar
 
     assets_root = os.path.abspath(game_directory + "\\assets")
     assets_index_name = ver_json["assetIndex"]["id"]
     auth_uuid = "0"
     auth_access_token = "0"
-    user_type = "Mojang"
+    user_type = "Legacy"
     version_type = '"First Minecraft Launcher Demo"'
     user_properties = "{}"
     auth_session = "{}"
     clientid = "0"
     auth_xuid = "0"
+    game_assets = "resources"
 
     cp = ""
     for i in cps:
@@ -92,5 +65,3 @@ def launch(game_directory: str = ".minecraft", version_name: str = None, java: s
 
         return f'{temp} {cp}{os.path.realpath(f"{game_directory}/versions/{version_name}/{version_name}.jar")} {ver_json["mainClass"]} ' + eval(
             'f"' + gethighoptions(ver_json["arguments"]["game"]) + '"')
-
-
