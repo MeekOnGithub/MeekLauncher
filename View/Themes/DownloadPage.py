b9a647564e80d58f0c6e7dfd7efc433f9e7761a2
Sharll-large <106537475+Sharll-large@users.noreply.github.com>
2022-11-16 21:19:45 +0800

    Add files via upload

---
+++ View/Themes/DownloadPage.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import tkinter.ttk
+import Core.System.CoreConfigIO
+import Core.Download.MinecraftVanillaDownload
+import Core.System.CoreVersionGet
+
+
+def main():
+    print(__file__+": load download page")
+
+    def downmc():
+        dotmc = Core.System.CoreConfigIO.read()[".mc"]
+
+        Core.Download.MinecraftVanillaDownload.download(download_as.get(), download_versions.get(), dotmc, int(
+            Core.System.CoreConfigIO.read()["threads"]))
+        print("Ok")
+
+    def get_versions():
+        vers = []
+        if r.get() == 1:
+            vers.append("release")
+        if b.get() == 1:
+            vers.append("snapshot")
+        if o.get() == 1:
+            vers.append("old_alpha")
+        download_versions["values"] = Core.System.CoreVersionGet.get_version_list(vers)
+
+    def auto_fill_download_as(a):
+        print(a)
+        download_as.delete(0, tk.END)
+        download_as.insert(0, download_versions.get())
+
+    download_view = tk.ttk.Frame()
+
+    download_versions_text = tk.ttk.Label(download_view, text="Game version:")
+    download_versions_text.place(x=10, y=10)
+
+    r = tk.IntVar()
+    if_release = tk.ttk.Checkbutton(download_view, text="Release", variable=r)
+    if_release.place(x=10, y=40)
+
+    b = tk.IntVar()
+    if_beta = tk.ttk.Checkbutton(download_view, text="Snapshot", variable=b)
+    if_beta.place(x=80, y=40)
+
+    o = tk.IntVar()
+    if_old_alpha = tk.ttk.Checkbutton(download_view, text="Old Alpha", variable=o)
+    if_old_alpha.place(x=160, y=40)
+
+    download_versions = tk.ttk.Combobox(download_view, postcommand=get_versions)
+    download_versions.bind('<<ComboboxSelected>>', auto_fill_download_as)
+    download_versions.place(x=100, y=10)
+
+    download_as_text = tk.ttk.Label(download_view, text="Download as:")
+    download_as_text.place(x=10, y=70)
+
+    download_as = tk.ttk.Entry(download_view)
+    download_as.place(x=100, y=70)
+
+    download_button = tk.ttk.Button(download_view, text="Download", command=downmc)
+    download_button.place(x=10, y=200)
+
+    return download_view
---
+++ View/core.py
@@ -0,0 +1,32 @@
+import os
+import tkinter as tk
+import tkinter.filedialog
+import tkinter.messagebox
+import tkinter.ttk
+
+import View.Themes.DownloadPage
+import View.Themes.LaunchPage
+import View.Themes.SettingPage
+import View.Themes.AccountManagePage
+import View.Themes.AboutPage
+
+
+def main():
+    print("Using FMCL GUI core 1.0 by Sharll.")
+    root = tk.Tk()
+    root.geometry('400x300')
+    root.title('First Minecraft Launcher')
+    root.resizable(0, 0)
+    root.iconbitmap(os.path.join(os.path.dirname(__file__), "icon.ico"))
+
+    notebook = tk.ttk.Notebook(root)
+
+    notebook.add(View.Themes.LaunchPage.main(), text='Launch')
+    notebook.add(View.Themes.DownloadPage.main(), text='Download')
+    notebook.add(View.Themes.SettingPage.main(), text='Settings')
+    notebook.add(View.Themes.AccountManagePage.main(), text="Accounts")
+    notebook.add(View.Themes.AboutPage.main(), text="About")
+
+    notebook.pack(padx=10, pady=10, fill=tkinter.BOTH, expand=True)
+
+    root.mainloop()
